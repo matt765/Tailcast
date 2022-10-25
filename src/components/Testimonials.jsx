2bbc8f294111a84a5a0d64c671338f8c7b146a77
matt765 <mateusz.wyrebek@gmail.com>
2022-10-25 13:30:51 +0200

    Make testimonials responsive

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 
 export const Testimonials = () => (
   <section className="w-full flex justify-center pt-10 mb-32 bg-customDarkBg2">
-    <div className="flex flex-col w-3/5 justify-center">
+    <div className="flex flex-col w-full lg:w-[1150px] justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
@@ -17,12 +17,12 @@ export const Testimonials = () => (
         <div className="custom-block-subtitle text-center mb-6">
           Lorem Ipsum
         </div>
-        <div className="custom-block-big-title text-center mb-16">
+        <div className="custom-block-big-title text-center mb-16 px-8 sm:px-24 md:px-48">
           People like you love our product
         </div>
 
-        <div className="flex gap-10">
-          <div className="w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
+          <div className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
             <div className="flex mb-2">
               <QuoteIcon />
             </div>
@@ -33,7 +33,7 @@ export const Testimonials = () => (
               customers what we’re working on, as well as give them the freedom
               to suggest features and vote on it.”
             </div>
-            <div className="flex mt-8 mb-4">
+            <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
               <img src={testimonial1} alt="" width="45px" />
               <div className="flex flex-col ml-4">
                 <div className="custom-content-text-white font-medium">
@@ -45,7 +45,8 @@ export const Testimonials = () => (
               </div>
             </div>
           </div>
-          <div className="w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
+          <div className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
+
             <div className="flex mb-2">
               <QuoteIcon />
             </div>
@@ -56,7 +57,7 @@ export const Testimonials = () => (
               customers what we’re working on, as well as give them the freedom
               to suggest features and vote on it.”
             </div>
-            <div className="flex mt-8 mb-4">
+            <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
               <img src={testimonial2} alt="" width="45px" />
               <div className="flex flex-col ml-4">
                 <div className="custom-content-text-white font-medium">
@@ -68,7 +69,8 @@ export const Testimonials = () => (
               </div>
             </div>
           </div>
-          <div className="w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
+          <div className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4">
+
             <div className="flex mb-2">
               <QuoteIcon />
             </div>
@@ -79,7 +81,7 @@ export const Testimonials = () => (
               customers what we’re working on, as well as give them the freedom
               to suggest features and vote on it.”
             </div>
-            <div className="flex mt-8 mb-4">
+            <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
               <img src={testimonial3} alt="" width="45px" />
               <div className="flex flex-col ml-4">
                 <div className="custom-content-text-white font-medium">
