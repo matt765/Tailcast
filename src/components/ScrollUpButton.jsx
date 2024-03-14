86d82a89af3ef958524a6314f7bfa955240caa5f
para360 <paras@paras360.com>
2024-03-14 15:53:33 -0500

    changed the scroll up bar to be way better

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export const ScrollUpButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
   }, []);
 
   const toggleVisible = () => {
@@ -23,19 +27,33 @@ export const ScrollUpButton = () => {
     });
   };
 
+  const buttonVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <>
       {isVisible && (
-        <div
-          className="w-12 h-12 fixed bottom-6 right-6 custom-border-gray rounded-xl  bg-customDarkBg2 hover:bg-customDarkBg3 cursor-pointer flex justify-center items-center transition z-50"
+        <motion.div
+          className="w-12 h-12 fixed bottom-6 right-6 custom-border-gray rounded-full bg-customDarkBg2 hover:bg-customDarkBg3 cursor-pointer flex justify-center items-center transition z-50"
           onClick={scrollToTop}
+          initial="hidden"
+          animate="visible"
+          variants={buttonVariants}
+          transition={{ duration: 0.3 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
         >
-          <svg
+          <motion.svg
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             width="35px"
             height="35px"
             viewBox="0 0 20 20"
+            initial={{ rotate: 180 }}
+            animate={{ rotate: 0 }}
+            transition={{ duration: 0.3 }}
           >
             <path
               d="M4.16732 12.5L10.0007 6.66667L15.834 12.5"
@@ -44,8 +62,8 @@ export const ScrollUpButton = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
             ></path>
-          </svg>
-        </div>
+          </motion.svg>
+        </motion.div>
       )}
     </>
   );
